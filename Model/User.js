60c980cecb3bf0a5704c9d27fe9e486b5d7f4eed
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-06 06:47:24 +0100

    JWT for password encryption on User Model

--- Model/User.js
+++ Model/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -37,4 +40,26 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT
+userSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({
+        user_id: this._id
+    }, process.env.JWT_SECRET)
+}
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('users', userSchema);
