26fbd82df8cfe32295a0df41d681384f15543e52
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-06 18:29:38 +0100

    Added Send Request

---
+++ Utils/sendRequest.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+// function sendRequest(error, req, res, next) {
+
+// }
+const sendRequest = async (url, method, data, token) => {
+    var base_uri = "https://api.sandbox.sudo.cards";
+    var headers = {
+        'Authorization': 'Bearer ' + process.env.SUDO_KEY
+    }
+
+    const request = await axios({
+            method: method,
+            url: base_uri + url,
+            headers: headers,
+            data: data
+        })
+        .then(response => {
+            // console.log(response.data);
+            return response.data;
+        });
+    // console.log(request);
+    return request;
+};
+
+module.exports = sendRequest;
