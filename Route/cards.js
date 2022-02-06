a93d05e054429f18289ede70c0abb8921a9d22df
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-06 18:54:17 +0100

    Applied Advanced Result

--- Route/cards.js
+++ Route/cards.js
@@ -5,16 +5,20 @@ const {
     getCard,
     createCard
 } = require('../Controller/cards');
-
+const Card = require('../Model/Card');
 const router = express.Router({ mergeParams: true });
+const advancedResults = require('../Middleware/advancedResults');
+
 const {
-    protect
+    protect,
+    authorize
 } = require('../Middleware/auth');
+
 // user routes
 router
     .route('/')
-    .get(protect, getCards)
-    .post(protect, createCard);
+    .get(protect, authorize('admin'), advancedResults(Card), getCards)
+    .post(protect, authorize('admin'), createCard);
 router
     .route('/:id')
     .get(getCard);
