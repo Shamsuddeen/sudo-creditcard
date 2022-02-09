01d3de64db86d939aa793c06d266d5f5619cdd5e
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-09 18:15:05 +0100

    Added Card Creation page

---
+++ client/src/views/CreateCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="about">
+    <DashboardCard />
+    <h1>Please wait while we create your Credit Card</h1>
+  </div>
+</template>
+<script>
+    import { HTTP } from "@/service/http-common";
+    export default {
+        name: "CreateCard",
+        data() {
+            return {
+                card: [],
+                user: [],
+                errors: [],
+            }
+        },
+        async created() {
+            await HTTP.post(`/auth/user`, {
+                token: localStorage.getItem('user')
+            })
+            .then(response => {
+                this.user = response.data.data;
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+
+            await HTTP.post(`/cards`, {
+                "user": this.user._id
+            })
+            .then(response => {
+                // console.log(response.data);
+                this.card = response.data.data;
+                window.location.href="./dashboard";
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })
+        }
+    }
+</script>
