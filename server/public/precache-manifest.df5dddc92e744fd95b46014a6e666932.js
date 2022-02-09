c2326e22f8d5f81e77c68efb03a7ae43e8242e63
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-09 22:10:59 +0100

    Added Vue Frontend to the server

---
+++ server/public/precache-manifest.df5dddc92e744fd95b46014a6e666932.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d665ebef7f4ee93cfc",
+    "url": "/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "3426b158aba3dfba03e9",
+    "url": "/css/chunk-vendors.8bda0bd5.css"
+  },
+  {
+    "revision": "fc5f3170ff7a8b97727550c99bb90283",
+    "url": "/img/logo.fc5f3170.png"
+  },
+  {
+    "revision": "a5a6a192e3497608b36fcfe335e1b2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcb52c700193e7a25217",
+    "url": "/js/about.fd83865f.js"
+  },
+  {
+    "revision": "b9d665ebef7f4ee93cfc",
+    "url": "/js/app.4f18941c.js"
+  },
+  {
+    "revision": "3426b158aba3dfba03e9",
+    "url": "/js/chunk-vendors.2bd54fe8.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
