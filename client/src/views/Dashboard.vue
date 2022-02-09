3772d935506ef70d6ef082fe4e3187c5d5adfb57
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-09 18:36:45 +0100

    Logout page

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -4,9 +4,11 @@
     <b-container class="bv-example-row">
     <b-row>
         <b-col>
+            <h1>Credit Card</h1>
             <UserCard />
         </b-col>
         <b-col>
+            <h1>Transactions</h1>
             <TransactionsCard />
         </b-col>
     </b-row>
---
+++ client/src/views/Logout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>Logout page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "Logout",
+  async created() {
+    await localStorage.removeItem("user");
+    window.location.href="./";
+  }
+};
+</script>
