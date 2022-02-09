10b6649c4b6769867a72e4172ff689607c6819af
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-09 18:15:28 +0100

    Added User Registration page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>Regsiter page</h1>
+    <RegisterForm />
+  </div>
+</template>
+<script>
+import RegisterForm from "@/components/RegisterForm.vue";
+
+export default {
+    name: "Register",
+    components: {
+        RegisterForm,
+    },
+}
+</script>
