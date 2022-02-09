cf0cdff617422570373340a653393e73ad693011
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-09 10:03:31 +0100

    Seperated client and server  side

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express       = require('express');
 const dotenv        = require('dotenv');
 const bodyParser    = require('body-parser');
 const mongoose      = require('mongoose');
+const cors          = require('cors');
 const morgan        = require('morgan');
 const cookieParser = require('cookie-parser');
 const errorHandler  = require('./Middleware/error');
@@ -28,6 +29,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 // Cookie parser
 app.use(cookieParser());
+app.use(cors());
 
 var port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Hello from Bitako.Cards'));
