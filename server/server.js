199cf9488dd65bd7161c3aa099df668d9328e985
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-11 00:21:25 +0100

    All GET changed to POST, Nah VUE Cause am...

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 // app.get('/', (req, res) => res.send('Hello from Bitako.Cards'));
 
 // Development logging
