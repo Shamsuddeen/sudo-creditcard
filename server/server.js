81cfb1d164d234b55a91346eda6ba2481924756e
Shamsuddeen Omacy <omacys2@gmail.com>
2022-02-11 00:22:03 +0100

    All GET changed to POST, Nah VUE Cause am...

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === 'development'){
 
 // Production
 // if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/public/'));
+    app.use(express.static(__dirname + '/public'));
     // handle spa
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 // }
